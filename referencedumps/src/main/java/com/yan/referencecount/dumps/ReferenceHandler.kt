efabfe0f976528342c96fd6e70daa1b56005843e
Bevan <xianwei.yan@quvideo.com>
2020-12-06 00:16:12 +0800

    feat: ReferenceHandler.kt 微调

--- referencedumps/src/main/java/com/yan/referencecount/dumps/ReferenceHandler.kt
+++ referencedumps/src/main/java/com/yan/referencecount/dumps/ReferenceHandler.kt
@@ -2,32 +2,20 @@ package com.yan.referencecount.dumps
 
 import android.os.Handler
 import android.os.HandlerThread
-import android.util.Log
 
 /**
  * @author Bevan (Contact me: https://github.com/genius158)
  * @since  2020/11/22
  */
 internal class ReferenceHandler {
-    fun post(runnable: ()->Unit) {
+    fun post(runnable: () -> Unit) {
         handler.post(runnable)
     }
 
-    private val handlerThread: HandlerThread
+    private val handlerThread: HandlerThread = HandlerThread("ReferenceMgr")
     private val handler: Handler
 
     init {
-        handlerThread = object : HandlerThread("ReferenceMgr") {
-            override fun run() {
-                try {
-                    super.run()
-                } catch (e: Throwable) {
-                    Log.e("ReferenceKernel", "ReferenceKernelError error --------------------")
-                    e.printStackTrace()
-                }
-            }
-        }
-
         handlerThread.start()
         handler = Handler(handlerThread.looper)
     }
