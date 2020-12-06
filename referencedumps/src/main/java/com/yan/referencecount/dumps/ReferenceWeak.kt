f746dc279c487bbf76e0ffe33297719a3f6c2e45
Bevan <xianwei.yan@quvideo.com>
2020-12-06 23:59:20 +0800

    docs: dump模块 ReferenceWeak.kt 新增 extra变量

--- referencedumps/src/main/java/com/yan/referencecount/dumps/ReferenceWeak.kt
+++ referencedumps/src/main/java/com/yan/referencecount/dumps/ReferenceWeak.kt
@@ -7,11 +7,14 @@ import java.lang.ref.WeakReference
  * @since  2020/11/21
  */
 class ReferenceWeak<T>(target: T, val stack: String?) : WeakReference<T>(target) {
+    var extra: Any? = null
+
     override fun hashCode(): Int {
         return get()?.hashCode() ?: 0
     }
 
     override fun equals(other: Any?): Boolean {
-        return get() == (other as? ReferenceWeak<*>)?.get()
+        val equals = super.equals(other)
+        return equals || get() == (other as? ReferenceWeak<*>)?.get()
     }
 }
